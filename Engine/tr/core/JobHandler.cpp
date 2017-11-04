fa92afd8c30b681bbb14b21aeb1553f2ba70eefa
manimax3 <manimax3@hotmail.de>
2017-11-04 21:55:55 +0100

    JobHandler added timeout
        + Fixed JobHandler test case

--- Engine/tr/core/JobHandler.cpp
+++ Engine/tr/core/JobHandler.cpp
@@ -31,9 +31,11 @@ bool JobHandler::Initialize(Engine *engine)
 
             while(mRunning)
             {
-                mQueue.wait_dequeue(func);
-                EASY_BLOCK("Task");
-                func();
+                if(mQueue.wait_dequeue_timed(func, std::chrono::microseconds(5)))
+                {
+                    EASY_BLOCK("Task");
+                    func();
+                }
             }
             
 			mActiveThreads--;
@@ -53,7 +55,7 @@ bool JobHandler::Shutdown()
     // Make sure that each thread leaves the while loop
     for(int i = 0; i < std::thread::hardware_concurrency() + 4; i++)
         AddJob([](){
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         });
     
 	for(auto& thread : mThreadPool)
--- Tests/jobhandler_test.cpp
+++ Tests/jobhandler_test.cpp
@@ -1,26 +1,36 @@
 #include "catch.hpp"
-#include "tr/core/JobHandler.h"
+#include "tr/core/Engine.h"
+#include "tr/core/Subsystem.h"
+
+int i = 0;
+
+class TestJobHandlder : public tr::Subsystem<TestJobHandlder>
+{
+public:
+    bool Initialize(tr::Engine *engine) override {
+        
+        return BaseSubsystem::Initialize(engine);
+    }
+    
+    bool Tick() override {
+    
+        auto* jh = mEngine->GetSystem<tr::JobHandler>();
+        jh->AddJob([&](){
+            i = 10;
+        });
+        
+        mEngine->Stop();
+        
+        REQUIRE(i == 10);
+        
+        return BaseSubsystem::Tick();
+    }
+};
+
 
 TEST_CASE("JobHandler", "[jobs]")
 {
-	// Create a JobHandler
-	/*tr::JobHandler handler;
-
-	// Start the handler right away
-	handler.Initialize(nullptr);
-
-	SECTION("Adding a task")
-	{
-		int i = 0;
-
-		auto status = handler.AddJob([&](){
-			i = 10;
-		});
-
-		handler.Shutdown();
-
-		REQUIRE(status);
-		REQUIRE(i == 10);
-
-	}*/
+	tr::Engine engine;
+    engine.AddSystem<TestJobHandlder>();
+    engine.Start();
 }
