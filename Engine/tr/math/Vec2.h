1f1632921c7e06f20aae824eefa6f428e1793570
manimax3 <manimax3@hotmail.de>
2018-03-11 17:50:33 +0100

    Vec2

--- Engine/tr/math/Vec2.h
+++ Engine/tr/math/Vec2.h
@@ -2,13 +2,93 @@
 #include <tr.h>
 
 namespace tr {
-class TR_API Vec2 {
+class Vec2 {
 public:
-    union {
-        float m_Values[2];
-        struct {
-            float x, y;
-        };
-    };
+    Vec2(float v = 0.f)
+        : x(v)
+        , y(v)
+    {
+    }
+    Vec2(float x, float y)
+        : x(x)
+        , y(y)
+    {
+    }
+
+    Vec2 operator+(const Vec2 &other) const
+    {
+        return Vec2(x + other.x, y + other.y);
+    }
+    Vec2 operator+(float v) const { return Vec2(x + v, x + v); }
+
+    Vec2 operator-(const Vec2 &other) const
+    {
+        return Vec2(x - other.x, y - other.y);
+    }
+    Vec2 operator-(float v) const { return Vec2(x - v, x - v); }
+
+    Vec2 operator*(float v) const { return Vec2(x * v, x * v); }
+    Vec2 operator/(float v) const { return Vec2(x / v, x / v); }
+
+    Vec2 &Normalize()
+    {
+        const float l = Length();
+        x /= l;
+        y /= l;
+        return *this;
+    }
+
+    Vec2 Normalized() const
+    {
+        Vec2 v = *this;
+        v.Normalize();
+        return v;
+    }
+
+    float Length() const { return std::sqrt(x * x + y * y); }
+
+    Vec2 &operator+=(const Vec2 &other)
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+    Vec2 &operator+=(float v)
+    {
+        x += v;
+        y += v;
+        return *this;
+    }
+
+    Vec2 &operator-=(const Vec2 &other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+    Vec2 &operator-=(float v)
+    {
+        x -= v;
+        y -= v;
+        return *this;
+    }
+
+    Vec2 &operator*=(float v)
+    {
+        x *= v;
+        y *= v;
+        return *this;
+    }
+    Vec2 &operator/=(float v)
+    {
+        x /= v;
+        y /= v;
+        return *this;
+    }
+
+    float Dot(const Vec2 &other) const { return x * other.x + y * other.y; }
+
+public:
+    float x, y;
 };
 }
